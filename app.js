83740c53413e5293c312d2bf498d81ea43d1ab2f
parimal_tank <parimaltank2001@gmail.com>
2023-09-12 08:44:44 +0530

    Database Relationship in graphql

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
 const app = express();
 
 // Write a middleware handling the graphql query
-app.use("/graphql", graphqlHTTP({
-    schema
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
   console.log("Server is Running on 4000......");
--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,33 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 // Defining the Schema
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+} = graphql;
+
+//Dummy data for lazy ones :)
+
+const books = [
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
+  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
+  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" },
+];
+
+const authors = [
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" },
+];
 
 // This Query is Also Known as RIP Query
 
@@ -12,19 +37,72 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      // Add Relationship between author and books
+      type: AuthorType,
+      resolve(parent, args) {
+        console.log("parent: ", parent);
+        return _.find(authors, { id: parent.authorId });
+      },
+    },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    // If we remove the call back from the files and make it as object so that having a issue of 22 catch. But because they are objects that are not resolved until run-time, we get the catch-22 problem and hence have to wrap them inside of functions.
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        console.log("parent: ", parent);
+        return _.filter(books, { authorId: parent.id });
+
+        // return books.filter(bk => bk.authorId === parent.id)
+      },
+    },
   }),
 });
 
 // RootQuery means first request get here and inside that find particular schema, for example Book
 
-const RootQuery = new GraphQLType({
+const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } }, // args means same as find by id for book data
+      args: { id: { type: GraphQLString } }, // here we can use GraphQLID instead of GraphQLString of  // args means same as find by id for book data
+      // because this is only accept string id GraphQLString  so the type is always string
       resolve(parent, args) {
         // Code to get Data from the Database / other resources
+
+        // This is Find into a books array with given id
+        return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      // Author Type
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
+      },
+    },
+    books: {
+      // Get The List of Books
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      },
+    },
+    authors: {
+      // Get The List of Authors
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       },
     },
   },
